122d7b8c0767bd27b9af687fd49fdd891ec5c93e
mockingbird <mihazyx@yandex.ru>
2024-02-21 20:19:41 +0300

    rm config

--- config.py
+++
@@ -1,6 +0,0 @@
-db_user='backup_user'
-db_passwd='Jnshgdtw64'
-db_host='10.0.32.199'
-database='backup_database'
-remote_backup_table='remote_backup'
-PROX_LIST = [{'prox_name':'','prox_storage':''}]
